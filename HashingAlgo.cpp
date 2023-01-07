26cca61867117e7d9cfd7d10d5477f9902a0976a
Huzaifa <zaifali785@gmail.com>
2023-01-07 07:58:36 +0500

    Data File Added

--- HashingAlgo.cpp
+++ HashingAlgo.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
-#include <cmath>
 #include "FunctionControllers.h"
-
+#include <fstream>
+#include <string>
 main()
 {
-    HashTable NADRA(10);
-    int arr[] = {11111, 223233, 322432532, 4364387, 5786323, 63698294, 7742874, 834343249, 93143232, 102252441, 153541, 267628, 289829};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    AddCNIC(NADRA, arr, size);
-    AddCNIC(NADRA, 43);
+    HashTable NADRA(10); // initialize Object of hash table
+    // Read CNIC from .txt file and add it to the hash table
+    int word;
+    ifstream file("Data.txt");
+    if (file.is_open())
+    {
+        while (!file.eof())
+        {
+            file >> word;
+            AddCNIC(NADRA, word);
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Unable to open file." << endl;
+    }
     ShowAllData(NADRA);
-    cout << endl;
-    SearchCNIC(NADRA, 21111);
-    DeleteCNIC(NADRA, 223233);
-    ShowAllData(NADRA);
-    cout << endl;
-    NADRA.Update(11111, 222221);
-    ShowAllData(NADRA);
+    // SearchCNIC(NADRA, 11111);
+    // UpdateCNIC(NADRA, 11111 , 22222);
+    // DeleteCNIC(NADRA, 22222);
 }
